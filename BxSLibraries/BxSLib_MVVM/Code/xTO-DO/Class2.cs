814b4c0a1e519b896435e181eaed6f65c77d51ff
Derrick Bingham <dbcm@mweb.co.za>
2017-09-16 16:27:21 +0200

    update

--- BxSLibraries/BxSLib_MVVM/Code/xTO-DO/Class2.cs
+++ BxSLibraries/BxSLib_MVVM/Code/xTO-DO/Class2.cs
@@ -5,13 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BxSLib_MVVM.Code.Observable_Collection
+namespace BxSLib_MVVM.Code
 	{
 	class Class2
 		{
 		}
 	}
 
+//=================================================================================================
+
+//static AutoResetEvent MeasureMessageEvent = new AutoResetEvent(true);
+
+//private static void HandleMeasurementMessage(IMessage<MessageEnvelope> msg)
+//{
+//    /* Do a bunch of stuff to msg */
+
+//    // Wait for exclusive access
+//    MeasureMessageEvent.WaitOne();
+
+//    EventHubDataBatch.Push(eventHubDatum);
+
+//    if(EventHubDataBatch.Count == 100)
+//    {
+//       /* Send them off*/
+//       EventHubDatabatch.Clear();
+//    }
+
+//    // Release exclusive access
+//    MeasureMessageEvent.Set();
+//}
+
+//=================================================================================================
 
 //[Export]
 //		public class LogFeedViewModel : BaseViewModel
